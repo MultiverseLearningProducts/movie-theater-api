72c6e1450376bad54d93b5d2a07f68b5158b50af
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-10 16:27:06 +0000

    added error handling to user requests

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
+const findUser = require("../middleware/usersMiddleware");
 const { User, Show } = require("../models");
-
 const userRouter = Router();
 
 // GET all users
@@ -11,24 +11,44 @@ userRouter.get("/", async (req, res) => {
 });
 
 // GET one user - using an endpoint
+// if user does not exist, return 404 not found status
 userRouter.get("/:id", async (req, res) => {
   const singleUser = await User.findByPk(req.params.id);
-  res.send(singleUser);
+  if (!singleUser) {
+    res.status(404).send("User not found");
+  } else {
+    res.send(singleUser);
+  }
 });
 
 // GET all shows watched by one user
+// if user does not exist, return 404 not found status
+// if user has no watched shows, return 404 not found status
 userRouter.get("/:id/shows", async (req, res) => {
   const singleUser = await User.findByPk(req.params.id);
-  const singleUserShows = await singleUser.getShows({
-    where: { status: "watched" },
-  });
-  res.send(singleUserShows);
+  if (!singleUser) {
+    return res.status(404).send("User not found");
+  } else {
+    const singleUserShows = await singleUser.getShows({
+      where: { status: "watched" },
+    });
+    if (singleUserShows.length === 0) {
+      res.status(404).send(`No shows found for ${singleUser.username}`);
+    } else {
+      res.send(singleUserShows);
+    }
+  }
 });
 
 // PUT update and add a show if a user has watched it
+// if user does not exist, return 404 not found status
+// if show does not exist, return 404 not found status
 userRouter.put("/:userId/shows/:showId", async (req, res) => {
   const singleUser = await User.findByPk(req.params.userId);
   const showToUpdate = await Show.findByPk(req.params.showId);
+  if (!singleUser || !showToUpdate) {
+    return res.status(404).send("Not found");
+  }
   const updatedShow = await showToUpdate.update(req.body, {
     where: {
       id: req.params.showId,
