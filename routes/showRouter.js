b1b3a5cd323bbf9c331bd523f15410ae8d8dcc14
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-10 16:35:31 +0000

    Add put and delete functionality to user and show routes

--- routes/showRouter.js
+++ routes/showRouter.js
@@ -11,7 +11,7 @@ showRouter.get("/", async (req, res) => {
     }
     res.status(200).json(shows);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(400).send(error.message);
   }
 });
 
@@ -24,6 +24,61 @@ showRouter.get("/:id", async (req, res) => {
     res.status(200).json(show);
   } catch (error) {
     res.status(400).send(error.message);
+  }
+});
+
+showRouter.get("/genres/:genre", async (req, res) => {
+  try {
+    const showWithGenre = await Show.findAll({
+      where: { genre: req.params.genre },
+    });
+    if (!showWithGenre.length) {
+      throw new Error("No shows found with this genre.");
+    }
+    res.status(200).json(showWithGenre);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+showRouter.put("/:id/watched", async (req, res) => {
+  try {
+    const watchedShow = await Show.findOne({ where: { id: req.params.id } });
+    if (!watchedShow) {
+      throw new Error("Show not found in db");
+    }
+    watchedShow.rating = req.body.rating;
+    await watchedShow.save();
+    res.status(200).send("Show rating successfully updated");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+showRouter.put("/:id/updates", async (req, res) => {
+  try {
+    const statusShow = await Show.findOne({ where: { id: req.params.id } });
+    if (!statusShow) {
+      throw new Error("Show not found in db");
+    }
+    statusShow.status = req.body.status;
+    await statusShow.save();
+    res.status(200).send("Show status successfully updated");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+showRouter.delete("/:id", async (req, res) => {
+  try {
+    const showToDelete = await Show.findOne({ where: { id: req.params.id } });
+    if (!showToDelete) {
+      throw new Error("The show you would like to delete does not exist");
+    }
+    showToDelete.destroy();
+    res.status(200).send("Show removed.");
+  } catch (error) {
+    res.status(404).send(error);
   }
 });
 
