cede540285eb2b29622bdef70ec81f2a192db51f
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-10 21:53:32 +0000

    Show router requests made

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const seed = require("../seed");
 const userRouter = require("../routes/user");
+const showRouter = require("../routes/shows");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", userRouter);
+app.use("/shows", showRouter);
 
 app.listen(5001, async () => {
   await seed();
