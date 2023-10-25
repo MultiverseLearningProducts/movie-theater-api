d1cb93bac04ee5a685cf0648358be689a3d9901f
stevenjlance <steven.j.lance@gmail.com>
2023-10-25 09:39:17 -0400

    fixed genre data type

--- models/Show.js
+++ models/Show.js
@@ -4,7 +4,7 @@ const { db, DataTypes } = require('../db/connection')
 //Creating a User child class from the Model parent class
 const Show = db.define("shows", {
     title: DataTypes.STRING,
-    genre: DataTypes.ENUM("Comedy", "Drama", "Horror", "Sitcom"),
+    genre: DataTypes.STRING,
     rating: DataTypes.INTEGER,
     available: DataTypes.BOOLEAN,
 });
